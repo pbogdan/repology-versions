d032da513013e123901d7f20e58d4928c52af923
Piotr Bogdan <ppbogdan@gmail.com>
2020-06-20 16:01:11 +0100

    repology-versions: initial implementation

--- src/Repology/Versions.hs
+++ src/Repology/Versions.hs
@@ -1,1 +1,30 @@
-module Repology.Versions () where
+module Repology.Versions
+  ( Version(..)
+  , versionCompare
+  )
+where
+
+import qualified Data.ByteString               as Bytes
+import qualified Data.ByteString.Char8         as CharBytes
+import           Foreign.Ptr                    ( castPtr )
+import           Repology.Versions.FFI          ( c_version_compare2 )
+import           System.IO.Unsafe               ( unsafePerformIO )
+
+newtype Version = Version String deriving (Eq, Show)
+
+instance Ord Version where
+  compare = versionCompare
+
+compare' :: String -> String -> IO Int
+compare' v1 v2 = Bytes.useAsCString (CharBytes.pack v1) $ \v1ptr ->
+  Bytes.useAsCString (CharBytes.pack v2) $ \v2ptr ->
+    fromIntegral <$> c_version_compare2 (castPtr v1ptr) (castPtr v2ptr)
+
+versionCompare :: Version -> Version -> Ordering
+versionCompare (Version v1) (Version v2) =
+  let ret = unsafePerformIO $ compare' v1 v2
+  in  case ret of
+        -1 -> LT
+        0  -> EQ
+        1  -> GT
+        _  -> error "unexpected return value from c_version_compare2"
--- src/Repology/Versions/FFI.hs
+++ src/Repology/Versions/FFI.hs
@@ -1,1 +1,12 @@
-module Repology.Versions.FFI () where
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Repology.Versions.FFI
+  ( c_version_compare2
+  )
+where
+
+import           Foreign
+import           Foreign.C.Types
+
+foreign import ccall unsafe "libversion/version.h version_compare2"
+    c_version_compare2 :: Ptr CUChar -> Ptr CUChar -> IO CInt
